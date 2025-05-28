713a8b7fa6245d0b07ddfe20a9309c847e5a532e
XDUSTERPL <xdusterpl@gmail.com>
2025-05-28 13:18:28 +0200

    usuwanie/edycja komentarzy i reakcji

--- URgoat/src/main/java/demo/komentarz/KomentarzController.java
+++ URgoat/src/main/java/demo/komentarz/KomentarzController.java
@@ -1,7 +1,9 @@
 package demo.komentarz;
 
 import demo.SerwisAplikacji;
+import demo.post.Post;
 import demo.post.PostRepository;
+import demo.post.PostTransData;
 import demo.reakcja.ReakcjaRepository;
 import demo.uzytkownik.Uzytkownik;
 import demo.uzytkownik.UzytkownikRepository;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -68,4 +71,56 @@ public class KomentarzController {
         return "wyskom"; //Przekierowanie na strone
 
     }
+
+    @RequestMapping(value = "/usun_komentarz", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String usunPost(Model model, @RequestParam Long id) {
+        komentarzService.usunKomentarz(id);
+
+        model.addAttribute("header", "Wynik");
+        model.addAttribute("message","Komentarz został usunięty");
+
+        return "viewmessage";
+    }
+
+    @RequestMapping(value = "/edytuj_komentarz", method = RequestMethod.GET)
+    public String edytujKomentarz(Model model, @RequestParam Long id) {
+        if (id == null || id == 0) {
+            model.addAttribute("header", "Błąd");
+            model.addAttribute("message", "ID komentarza jest nieprawidłowe");
+            return "viewmessage";
+        }
+
+
+        Komentarz komentarz = komentarzRepository.findByKomentarzID(id);
+        KomentarzTransData transData = new KomentarzTransData();
+        transData.setKomentarzID(komentarz.getKomentarzID());
+        transData.setTresc(komentarz.getTresc());
+        model.addAttribute("transData", transData);
+        return "aktualizacja_komentarza";
+    }
+
+    @RequestMapping(value = "/edytuj_komentarz", method = RequestMethod.POST)
+    public String edytujKomentarz(Model model, KomentarzTransData komentarzTransData) {
+        String tresc = komentarzTransData.getTresc();
+        long id = komentarzTransData.getKomentarzID();
+
+        if (tresc == null || tresc.isBlank()) {
+            model.addAttribute("header", "Wynik");
+            model.addAttribute("message", "Komentarz nie może być pusty");
+            return "viewmessage";
+        }
+
+        try {
+            komentarzService.aktualizujKomentarz(id, tresc);
+            model.addAttribute("header", "Wynik");
+            model.addAttribute("message", "Komentarz został zaktualizowany");
+        } catch (RuntimeException e) {
+            model.addAttribute("header", "Błąd");
+            model.addAttribute("message", e.getMessage());
+        }
+
+        return "viewmessage";
+    }
+
+
 }
