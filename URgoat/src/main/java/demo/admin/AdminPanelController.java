36e684be1e4acbebae238b88e1f30bb1953a409d
Mark-MaZe <markmaze12341@gmail.com>
2025-05-28 21:21:04 +0200

    Raporty zliczające ogólną liczbę.

--- URgoat/src/main/java/demo/admin/AdminPanelController.java
+++ URgoat/src/main/java/demo/admin/AdminPanelController.java
@@ -8,6 +8,7 @@ import demo.log.DBLogService;
 import demo.log.LogOperacja;
 import demo.post.Post;
 import demo.post.PostRepository;
+import demo.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,6 +29,8 @@ public class AdminPanelController {
     private PostRepository postRepository;
     @Autowired
     private KomentarzRepository komentarzRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/panel")
     public String wyswietlPanel(){
@@ -63,5 +66,31 @@ public class AdminPanelController {
         List<Komentarz> comments = komentarzRepository.findAll();
         model.addAttribute("comments", comments);
         return "admin/list_komentarzy";
+    }
+
+    @GetMapping("/raporty")
+    public String wyswietlRaporty(){
+        return "admin/raporty_list";
+    }
+
+    @GetMapping("/raport_postow")
+    public String wyswietlRaportyPosty(Model model) {
+        long liczbaPostow = postRepository.count();
+        model.addAttribute("liczbaPostow", liczbaPostow);
+        return "admin/raporty_post";
+    }
+
+    @GetMapping("/raport_komentarzy")
+    public String wyswietlRaportyKomentarze(Model model) {
+        long liczbaKomentarzy = komentarzRepository.count();
+        model.addAttribute("liczbaKomentarzy", liczbaKomentarzy);
+        return "admin/raporty_kom";
+    }
+
+    @GetMapping("/raport_uzytkownikow")
+    public String wyswietlRaportyUzytkownikow(Model model) {
+        long liczbaZarejestrowanych = userRepository.count();
+        model.addAttribute("liczbaZarejestrowanych", liczbaZarejestrowanych);
+        return "admin/raporty_uzy";
     }
 }
